fc6125b41c1fd20bbdf02a789fb2f6d1e7c6385c
Anvay <vats98754@gapps.uwcsea.edu.sg>
2023-05-16 21:50:28 -0400

    Use mutex to make a global lock to stop data races

---
+++ thread5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+using namespace std;
+
+mutex gLock;
+static int shared_value = 0;
+
+void shared_value_increment() {
+    gLock.lock();
+    ++shared_value;
+    gLock.unlock();
+}
+
+int main() {
+    vector <thread> threads;
+    for (int i = 0; i < 100; ++i) {
+        threads.push_back(thread(shared_value_increment));
+    }
+    for (int i = 0; i < 100; ++i) {
+        threads[i].join();
+    }
+    cout << "Shared value: " << shared_value << endl;
+
+    return 0;
+}
